193a220d7abb931afd97876cb8dbf7db70fe2ccd
Avinash Ravindra Padale <avinashpadale01@gmail.com>
2024-02-12 23:57:45 +0530

    added datatypes

--- Js/01_Basic/02_DataType.js
+++ Js/01_Basic/02_DataType.js
@@ -1,3 +1,30 @@
 "use strict"; //treat all JS code as newer version (for all content in file)
 
 // alert( 3 + 3) // we are using nodejs, not browser
+//code readability should be high
+
+// console.log("9")
+// console.log("Avi")
+
+let name = "Avi"
+let age = 18
+let  isMarried = false;
+
+
+//premetive :
+// number  => 2^53
+// bigInt 
+// string +> "--" '--'
+// boolean  => true/false
+// null     => a special value that represents no value (value assign)
+// undefined => a special value that represents no value (value not assign) but it can hold the type of data
+// symbols => to find uniqueness
+
+
+//object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);   //object
+
+

51df6e2ec02b6fb12ed9b1a2145e6fef96539771
Avinash Ravindra Padale <avinashpadale01@gmail.com>
2024-02-13 00:39:29 +0530

    Operation updated in file

--- Js/01_Basic/03_convertionOperation.js
+++ Js/01_Basic/03_convertionOperation.js
@@ -19,6 +19,55 @@
 // "" => false 
 // "A" => true
 
-let someNum = 33
-let stringNum = String(someNum)
-console.log(stringNum, typeof stringNum);
+// let someNum = 33
+// let stringNum = String(someNum)
+// console.log(stringNum, typeof stringNum);
+
+
+//*************** OPERATION  ******************//
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "Hello ";
+// let str2 = "World!";
+// let str3 = str1 +  str2;
+// console.log(str3);
+// console.log(str1 + str2); // concatenation
+
+// console.log("2" + 2);
+// console.log(2 + "2");
+// console.log("2" + 2 + 2); // 222
+// console.log(2 + 2 + "2"); // 42
+// order wise perform operation
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2
+
+// let gameCount = 100
+// gameCount++; // ++a, a++
+// console.log(gameCount);
+
+// let x = 3;
+// const y = x++;
+// console.log(`x:${x}, y:${y}`);// Expected output: "x:4, y:3"
+// let a = 3;
+// const b = ++a;
+// console.log(`a:${a}, b:${b}`);// Expected output: "a:4, b:4"
+
+//The difference between " (double quotes) and ` (backticks) is that double quotes are used to specify strings
+//in many programming languages, while backticks are used to denote a template literal in JavaScript. 
+//Template literals support string interpolation and multiline strings, which allow for dynamic values and easier reading of code.
+

246468e56c469281f10e1308e5e5137b2fae7202
Avinash Ravindra Padale <avinashpadale01@gmail.com>
2024-02-13 00:53:03 +0530

    added comparison file with some info

---
+++ Js/01_Basic/04_comparation.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 =! 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log("Avi" < "A");
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// comparisons(> < >= <=) and equality check (==) work differently
+// comparison convert null to a number, treating as 0
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+//All false
+
+// === //its check value aswell datatype
+
+console.log(4 === '4'); //false because one is string other is int

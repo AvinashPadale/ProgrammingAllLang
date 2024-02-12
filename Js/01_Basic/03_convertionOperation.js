7b5d001f34032242092ab663a672905a0e9db8d7
Avinash Ravindra Padale <avinashpadale01@gmail.com>
2024-02-13 00:17:17 +0530

    convertionOperation file

---
+++ Js/01_Basic/03_convertionOperation.js
@@ -0,0 +1,24 @@
+// let score = "true"
+// console.log(typeof score);
+
+// let valueInNum = Number(score)
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  True => 1 or False => 0
+// null => 0
+
+// let isLoggedIn = "A";
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true or 0 => false
+// "" => false 
+// "A" => true
+
+let someNum = 33
+let stringNum = String(someNum)
+console.log(stringNum, typeof stringNum);

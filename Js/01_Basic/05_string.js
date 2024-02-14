e896cf6796a0407838026e6c239ae6a609a83903
Avinash Ravindra Padale <avinashpadale01@gmail.com>
2024-02-14 23:46:09 +0530

    learn some string

---
+++ Js/01_Basic/05_string.js
@@ -0,0 +1,32 @@
+/*
+const name = "Avinash"
+const repoCount = 10
+// console.log(name + repoCount + "Padale");
+console.log(`Hello my name is ${name} and repo Count is ${repoCount}`);//string interpolation
+const gameName = new String("GTA")
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('T'));
+*/
+/*
+const gameName = new String("Avinash_Padale")
+const newString = gameName.substring(0,4)
+console.log(newString);
+const string2 = gameName.slice(-14,2)
+console.log(string2);
+console.log(gameName.length);
+
+const string3 = "   ravindra   "
+console.log(string3)
+console.log(string3.trim())
+
+const url = "https://avinash.com/javascript%20is%20awesome/"
+const string1 = url.replaceAll("%20","-")
+
+console.log(string1.split('-'));
+
+*/

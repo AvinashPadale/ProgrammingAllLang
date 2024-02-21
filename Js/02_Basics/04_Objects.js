af8150ba0fd11b76aae14ae3bd49564e232356d7
Avinash Ravindra Padale <avinashpadale01@gmail.com>
2024-02-22 00:17:14 +0530

    some more object funtion learn but have to learn more

---
+++ Js/02_Basics/04_Objects.js
@@ -0,0 +1,51 @@
+// const tin = new Object() -singleton
+const  tout = {}
+tout.id = "A101"
+tout.name = "Avinash"
+tout.age=25;
+// console.log(tout);
+
+const anotherUser = {
+    email : "anotheruser@gmail.com",
+    fullName : {
+        userName : {
+            firstName : "Aviansh",
+            lastName : "Padale"
+        }
+    }
+}
+
+// console.log(anotherUser.fullName.userName.firstName);// differnent  way of accessing object property
+
+const obj1 = {1:"A",2:"B"}
+const obj2 = {3:"A",4:"B"}
+// const obj3 = Object.assign(obj1,obj2) // value store in obj1
+// console.log(obj1==obj3);
+// // const obj3 = [obj1,obj2]
+// const obj4 = Object.assign({},obj1,obj2)  // value store in obj4
+// console.log(obj1 == obj4);
+// console.log(obj4);
+// const obj5 = {...obj1,...obj2}
+// console.log(obj5);
+
+
+const users = [
+{
+    id : 1,
+    email : "a@gmail.com"
+},
+{
+    id : 2,
+    email : "b@gmail.com"
+},
+{
+},
+]
+
+
+console.log(tout); //{ id: 'A101', name: 'Avinash', age: 25 }
+console.log(Object.keys(tout));//[ 'id', 'name', 'age' ]
+console.log(Object.values(tout));//[ 'A101', 'Avinash', 25 ]
+console.log(Object.entries(tout));// [ [ 'id', 'A101' ], [ 'name', 'Avinash' ], [ 'age', 25 ] ]
+
+console.log(tout.hasOwnProperty('age')); // true

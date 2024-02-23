4b852ef350da539f4188cb9fb5bc7a23889f9e7b
Avinash Ravindra Padale <avinashpadale01@gmail.com>
2024-02-24 00:16:28 +0530

    passing values to funtion like and object

--- Js/03_Basics/01_Function.js
+++ Js/03_Basics/01_Function.js
@@ -16,14 +16,43 @@ function add(num1,num2){ // parameter2
 const result = add(5,7)
 // console.log(`result : ${result}`);
 
-function LoginUserName(username = "Sam"){
+function LoginUserName(username){
     if(username === undefined){   //if(!username){
         console.log("Enter user name");
         return
     }
     return `${username} Just logged in`
 }
-
-
 // console.log(LoginUserName("Avinash")); // Avinash Just logged in
-console.log(LoginUserName()); // undefine Just logged in
+// console.log(LoginUserName()); // undefine Just logged in
+
+function calcCartPrice(val1, val2, ...num1){// rest  operator ... 
+    return num1
+}
+// console.log(calcCartPrice(400,500,600,700,800));
+
+const user = {
+    username : "Avinash",
+    price : 199
+}
+
+function handleobject(anyobject){
+
+    console.log(`Username is ${anyobject.username}`);
+    console.log(`price of the product is ${anyobject.price}`);
+}
+
+// handleobject(user);
+// handleobject({
+//     username: "John Doe",
+//     price:300
+// })
+
+
+const myNewArray = [200,300,400,500]
+function return2ndValue(getArray){
+    return getArray[1];
+}
+// console.log(`The second value of array is ${return2ndValue(myNewArray)}`)
+// console.log(return2ndValue([100,200,300,4000]));
+

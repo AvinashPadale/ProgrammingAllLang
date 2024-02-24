8d361b2b65d8b4d26b7b44a198476157ffd379f1
Avinash Ravindra Padale <avinashpadale01@gmail.com>
2024-02-25 00:09:13 +0530

    scope use in if statement and variable function

--- Js/03_Basics/02_Scope.js
+++ Js/03_Basics/02_Scope.js
@@ -2,14 +2,48 @@
 
 let a = 100
 
-if(true) //block scope -  value should not go out side of block
+if(true) //block scope -  value should not go out side of block // we can use global value in it 
 {
 let a = 10
 const b = 20
 // var c = 30
-console.log(`inner ${a}`);
+// console.log(`inner ${a}`);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "avi"
+    function two ()  {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+
+if (true){
+    const username = "hitesh"
+    if(username === "hitesh"){
+        const website = " youtube"
+       // console.log(username+website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+//------------------------ interesting -------------------------------//
+addone(5)
+console.log(addone(5));
+function addone(num){ // basic function
+    return num + 1
+}
+
+// addtwo(5) // declare in variable(hold function)
+const addtwo = function(num){ // expression (variable can store json, function)
+    return num + 2
+}
